8b220906fe974eec05804d6fd0d2bd3aadfce4ed
Webysther Nunes <webysther@gmail.com>
2020-11-24 01:37:29 +0000

    Adiciuonado licença

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/**
+ * Programa que realiza o cadastro de pacientes com COVID-19
+ *
+ * @link      https://github.com/webysther/PIM4
+ * @author    Webysther Nunes <webysther@gmail.com>
+ * @license   https://github.com/webysther/PIM4/blob/master/LICENSE MIT
+ */
+
 #include <stdio.h>
 
 int main(void) {
