9dad89225add798cb17d51541f3b0988bdda2182
Webysther Nunes <webysther@gmail.com>
2020-11-24 05:17:59 +0000

    Ajustes para recuperar os dados de forma mais segura

--- main.c
+++ main.c
@@ -10,144 +10,155 @@
 #include <string.h>
 #include <time.h>
 
-int main(void) {
+int main( int argc, char * argv[] ) {
   char usuario[256];
   char senha[32];
 
-  printf("\nDica: usuário e senha é admin");
-
   // login do sistema
   for (;;){
-    printf("\nDigite seu usuário e senha para acessar o sistema:");
+    printf("\nDigite seu usuario e senha para acessar o sistema:");
 
     // usuário
-    printf("\nUsuário: ");
-    scanf("%255s", usuario);
+    printf("\n\nUsuario: ");
+    fgets(usuario, sizeof usuario, stdin);
+    usuario[strcspn(usuario, "\n")] = 0;
 
     // senha
-    printf("\nSenha: ");
-    scanf("%31s", senha);
-	
+    printf("Senha: ");
+    int p=0;
+    do{
+        senha[p]=getch();
+        if(senha[p]!='\r'){
+            printf("*");
+        }
+        p++;
+    }while(senha[p-1]!='\r');
+    senha[p-1]='\0';
+
     // pode ser alterado por um sistema de login e senha melhor
     if (strcmp(usuario, "admin") != 0 || strcmp(senha, "admin") != 0){
-      printf("\nUsuário e senha incorretos");
+      printf("\nUsuario e senha incorretos");
       continue;
     }
 
     break;
-	}
+  }
 
   char nome[1024];
-  int cpf;
-  int telefone;
-  char nascimento[] = "01/01/1970";
-  int ano_nascimento, mes_nascimento, dia_nascimento; 
+  char cpf[16];
+  char telefone[16];
+  char nascimento[16];
+  int ano_nascimento, mes_nascimento, dia_nascimento;
   char email[1024];
-  char diagnostico[] = "01/01/1970";
-  int ano_diagnostico, mes_diagnostico, dia_diagnostico; 
+  char diagnostico[16];
+  int ano_diagnostico, mes_diagnostico, dia_diagnostico;
   char comorbidade[1024];
 
   // endereço
   char rua[1024];
-  int numero;
+  char numero[16];
   char bairro[1024];
   char cidade[1024];
   char estado[1024];
-  int cep;
+  char cep[16];
 
   // cadastro de pacientes
-  printf("\nDigite as informações do paciente:");
+  printf("\n\nDigite as informacoes do paciente:");
 
-  printf("\nNome: ");
-  scanf("%1023s", nome);
+  printf("\n\nNome: ");
+  fgets(nome, sizeof nome, stdin);
+  nome[strcspn(nome, "\n")] = 0;
 
-  printf("\nCPF: ");
-  scanf("%d", &cpf);
+  printf("CPF (xxx.xxx.xxx-xx): ");
+  fgets(cpf, sizeof cpf, stdin);
+  cpf[strcspn(cpf, "\n")] = 0;
 
-  printf("\nTelefone: ");
-  scanf("%d", &telefone);
+  printf("Telefone (xx xxxxx-xxxx): ");
+  fgets(telefone, sizeof telefone, stdin);
+  telefone[strcspn(telefone, "\n")] = 0;
 
   // Endereço
-  printf("\nEndereço");
+  printf("\nEndereco");
 
-  printf("\nRua: ");
-  scanf("%1023s", rua);
+  printf("\n\nRua: ");
+  fgets(rua, sizeof rua, stdin);
+  rua[strcspn(rua, "\n")] = 0;
 
-  printf("\nNúmero: ");
-  scanf("%d", &numero);
+  printf("Numero: ");
+  fgets(numero, sizeof numero, stdin);
+  numero[strcspn(numero, "\n")] = 0;
 
-  printf("\nBairro: ");
-  scanf("%1023s", bairro);
+  printf("Bairro: ");
+  fgets(bairro, sizeof bairro, stdin);
+  bairro[strcspn(bairro, "\n")] = 0;
 
-  printf("\nCidade: ");
-  scanf("%1023s", cidade);
+  printf("Cidade: ");
+  fgets(cidade, sizeof cidade, stdin);
+  cidade[strcspn(cidade, "\n")] = 0;
 
-  printf("\nEstado: ");
-  scanf("%1023s", estado);
+  printf("Estado: ");
+  fgets(estado, sizeof estado, stdin);
+  estado[strcspn(estado, "\n")] = 0;
 
-  printf("\nCEP: ");
-  scanf("%d", &cep);
+  printf("CEP (xxxxx-xxx): ");
+  fgets(cep, sizeof cep, stdin);
+  cep[strcspn(cep, "\n")] = 0;
 
-  printf("\nData de nascimento: ");
+  printf("Data de nascimento (xx/xx/xxxx): ");
+  fgets(nascimento, sizeof nascimento, stdin);
+  nascimento[strcspn(nascimento, "\n")] = 0;
   sscanf(
-    nascimento, 
-    "%d/%d/%d", 
+    nascimento,
+    "%d/%d/%d",
     &dia_nascimento,
     &mes_nascimento,
     &ano_nascimento
   );
 
-  printf("\nE-mail: ");
-  scanf("%1023s", email);
+  printf("Email: ");
+  fgets(email, sizeof email, stdin);
+  email[strcspn(email, "\n")] = 0;
 
-  printf("\nData de diagnóstico: ");
-  sscanf(
-    diagnostico, 
-    "%d/%d/%d", 
-    &dia_diagnostico,
-    &mes_diagnostico,
-    &ano_diagnostico
-  );
+  printf("Data de diagnostico (xx/xx/xxxx): ");
+  fgets(diagnostico, sizeof diagnostico, stdin);
+  diagnostico[strcspn(diagnostico, "\n")] = 0;
 
-  printf("\nComorbidade: ");
-  scanf("%1023s", comorbidade);
+  printf("Comorbidade: ");
+  fgets(comorbidade, sizeof comorbidade, stdin);
+  comorbidade[strcspn(comorbidade, "\n")] = 0;
 
   FILE *covid;
   covid = fopen("covid.txt", "a");
+  // csv separado por |
   fprintf(
     covid,
-    "%s,%d,%d,%s,%d,%s,%s,%s,%d,%d,%d,%d,%s,%d,%d,%d,%s",
-    nome, 
-    cpf, 
+    "%s|%d|%d|%s|%d|%s|%s|%s|%d|%s|%s|%s|%s\n",
+    nome,
+    cpf,
     telefone,
     rua, numero, bairro, cidade, estado, cep,
-    ano_nascimento, mes_nascimento, dia_nascimento,
+    nascimento,
     email,
-    ano_diagnostico, mes_nascimento, dia_diagnostico,
+    diagnostico,
     comorbidade
   );
   fclose(covid);
 
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
+  int idade = tm.tm_year + 1900 - ano_nascimento;
 
-  if(tm.tm_year + 1900 - ano_nascimento > 65){
+  if(idade > 65){
+    printf("\nPaciente pertence ao grupo de risco!");
+
     FILE *risco;
     risco = fopen("risco.txt", "a");
-    fprintf(
-      risco,
-      "%s,%d,%d,%s,%d,%s,%s,%s,%d,%d,%d,%d,%s,%d,%d,%d,%s",
-      nome, 
-      cpf, 
-      telefone,
-      rua, numero, bairro, cidade, estado, cep,
-      ano_nascimento, mes_nascimento, dia_nascimento,
-      email,
-      ano_diagnostico, mes_nascimento, dia_diagnostico,
-      comorbidade
-    );
+    fprintf(risco, "%s,%d\n", cep, idade);
     fclose(risco);
+    return 0;
   }
-  
+
+  printf("\nPaciente sem risco, cadastro finalizado!");
+
   return 0;
 }
